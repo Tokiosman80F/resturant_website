fe890f303816846d73e6b36c1968d30221f1a9c0
Tokiosman80F <tokiosman96@gmail.com>
2024-02-06 02:39:02 +0600

    popular menu section complete filteout the popular item

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,20 +1,24 @@
+const MenuCard = ({ menu }) => {
+  return (
+    <div className="flex gap-4  justify-between">
+      {/* img */}
 
-const MenuCard = ({menu}) => {
-    return (
-        <div className="flex gap-4 bg-slate-100  justify-between">
-            {/* img */}
-            <div className="w-[118px] h-[104px]">
-            <img src={menu.image} alt=""  />
-            </div>
-            {/* detail */}
-            <div>
-                <p className='logo text-xl'>{menu.name}</p>
-                <p className='text-gray-500'>{menu.recipe}</p>
-            </div>
-            {/* price */}
-            <div className='text-[#BB8506] text-xl'>${menu.price}</div>
-        </div>
-    );
+      <img
+        src={menu.image}
+        alt=""
+        style={{ borderRadius: "0 200px 200px 200px" }}
+        className="w-[118px] h-[104px]"
+      />
+
+      {/* detail */}
+      <div>
+        <p className="logo text-xl">{menu.name}</p>
+        <p className="text-gray-500">{menu.recipe}</p>
+      </div>
+      {/* price */}
+      <div className="text-[#BB8506] text-xl">${menu.price}</div>
+    </div>
+  );
 };
 
 export default MenuCard;
--- src/pages/Home/MenuSection.jsx
+++ src/pages/Home/MenuSection.jsx
@@ -2,19 +2,39 @@ import { useEffect, useState } from "react";
 import MenuCard from "../../components/MenuCard";
 
 const MenuSection = () => {
-    const [menu,setMenu]=useState([])
+  const [menu, setMenu] = useState([]);
   useEffect(() => {
     fetch(`http://localhost:3000/allMenu`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Menu Data Fail to fetch From Menu Section");
+        }
+        return response.json();
+      })
       .then((data) => {
-         console.log(data);
-         setMenu(data)
+        const popularItems = data.filter(
+          (items) => items.category === "popular"
+        );
+        console.log("popular item =>", popularItems);
+        setMenu(popularItems);
+      })
+      .catch((error) => {
+        console.error("Error fetch in menu Data of Menu Section ", error);
       });
   }, []);
   return (
-    <div className="max-width-container">
-      {menu.map((item)=><MenuCard key={item._id} menu={item}></MenuCard>)}
-    </div>
+    <>
+      <div className="max-width-container my-10">
+        <div className="grid grid-cols-2 gap-10">
+          {menu.map((item) => (
+            <MenuCard key={item._id} menu={item}></MenuCard>
+          ))}
+        </div>
+        <div className="text-center mt-12">
+        <button className="border-black border-b-4 rounded-lg px-5 py-2 uppercase bg-slate-100  hover:bg-yellow-400">View full menu</button>
+        </div>
+      </div>
+    </>
   );
 };
 
